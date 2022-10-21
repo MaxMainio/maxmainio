b56b51b4d1a76fc7d8ee4bbd7ea6cfc1dea16831
MaxMainio <beidler.max@gmail.com>
2022-10-21 14:23:52 -0400

    index added

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+console.log("https://www.youtube.com/watch?v=NuAKnbIr6TE");
+
+const indexBtn = document.querySelector("#index-btn")
+const indexSection = document.querySelector("#index-section")
+
+indexBtn.addEventListener("click", e => {
+	indexSection.classList.toggle("hide")
+})
