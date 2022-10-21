b45af8c32e73224655150c6f0fbfcbccca617eec
MaxMainio <beidler.max@gmail.com>
2022-10-21 16:19:02 -0400

    about modals working

--- about/scripts.js
+++ about/scripts.js
@@ -20,17 +20,51 @@ buttons.forEach(button => {
 })
 
 
-const xpBtn = document.querySelector(".xp-btn")
+// const xpBtn = document.querySelector(".xp-btn")
 
-xpBtn.addEventListener("click", e => {
-	var parent = xpBtn.closest(".xp-div")
-	var xpText = parent.querySelector(".xp-text")
+// xpBtn.addEventListener("click", e => {
+// 	var parent = xpBtn.closest(".xp-div")
+// 	var xpText = parent.querySelector(".xp-text")
 
-	xpText.classList.toggle("hide");
+// 	xpText.classList.toggle("hide");
 
-	if (xpBtn.innerHTML === "More...") {
-		xpBtn.innerHTML = "Less...";
-	} else {
-		xpBtn.innerHTML = "More...";
+// 	if (xpBtn.innerHTML === "More...") {
+// 		xpBtn.innerHTML = "Less...";
+// 	} else {
+// 		xpBtn.innerHTML = "More...";
+// 	}
+// })
+
+// var allXpBtns = document.getElementsByClassName("xp-btn");
+
+// for (var i = allXpBtns.length-1; i >=0  ; i--) {
+// 	(function () {
+// 		var modal = allXpBtns[i].id + "_modal"
+// 		allXpBtns[i].addEventListener("mouseenter", function() { 
+// 			document.getElementById(modal).style.display = "block"; }, false)
+// 		allXpBtns[i].addEventListener("mouseleave", function() { 
+// 			document.getElementById(modal).style.display = "none"; }, false)
+// 	}()); // immediate invocation
+// 	}
+
+var allXpBtns = document.getElementsByClassName("xp-btn");
+
+for (var i = allXpBtns.length-1; i >=0  ; i--) {
+	(function () {
+		var modal = allXpBtns[i].id + "_modal"
+		allXpBtns[i].addEventListener("click", function() { 
+			document.getElementById(modal).classList.toggle("hide")
+			
+			if (allXpBtns[i].innerHTML === "More..."){
+				allXpBtns[i].innerHTML = "Less...";
+			} else {
+				allXpBtns[i].innerHTML = "More...";
+			}
+		
+
+		
+		}, false)
+
+
+	}());
 	}
-})
