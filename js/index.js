beceb7d3f9e43cbd26b680bebb8682f4786d1529
MaxMainio <beidler.max@gmail.com>
2022-10-23 22:33:37 -0400

    splash added

--- js/index.js
+++ js/index.js
@@ -8,3 +8,16 @@ indexBtn.addEventListener("click", e => {
 })
 
 document.querySelector("#crosswalk-projection").playbackRate = 0.20;
+
+
+
+const splash = document.querySelector("#splash")
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		entry.target.classList.toggle("hide", entry.isIntersecting === false)
+	})
+	console.log(entries)
+})
+
+observer.observe(splash)
