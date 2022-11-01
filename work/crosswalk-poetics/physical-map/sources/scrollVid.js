e03d407e971602e186c8ff82851cbcf0f123ba96
MaxMainio <beidler.max@gmail.com>
2022-11-01 15:31:27 -0400

    crosswalk poetics added

--- work/crosswalk-poetics/physical-map/sources/scrollVid.js
+++ work/crosswalk-poetics/physical-map/sources/scrollVid.js
@@ -1,25 +1,22 @@
 console.log("https://www.youtube.com/watch?v=NuAKnbIr6TE");
 
 var frameNumber = 0, // start video at frame 0
-			// lower numbers = faster playback
-			playbackConst = 500, 
-			// get page height from video duration
-			setHeight = document.getElementById("set-height"), 
-			// select video element         
-			vid = document.getElementById('v0'); 
-			// var vid = $('#v0')[0]; // jquery option
+playbackConst = 500, // lower numbers = faster playback
+setHeight = document.getElementById("set-height"), // get page height from video duration			
+vid = document.getElementById('v0'); // select video element
 
-		// dynamically set the page height according to video length
-		vid.addEventListener('loadedmetadata', function() {
-		setHeight.style.height = Math.floor(vid.duration) * playbackConst * 2.8 + "px";
-		});
+// dynamically set the page height according to video length
+vid.addEventListener('loadedmetadata', function() {
+	setHeight.style.height = Math.floor(vid.duration) * playbackConst * 2.8 + "px";
+	console.log(Math.floor(vid.duration));
+});
 
 
-		// Use requestAnimationFrame for smooth playback
-		function scrollPlay(){  
-		var frameNumber  = window.pageYOffset/playbackConst;
-		vid.currentTime  = frameNumber;
-		window.requestAnimationFrame(scrollPlay);
-		}
+// Use requestAnimationFrame for smooth playback
+function scrollPlay(){  
+	var frameNumber  = window.pageYOffset/playbackConst;
+	vid.currentTime  = frameNumber;
+	window.requestAnimationFrame(scrollPlay);
+}
 
-		window.requestAnimationFrame(scrollPlay);
+window.requestAnimationFrame(scrollPlay);
