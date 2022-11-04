a975c51db2090c138eeb736d4101ed68c1b28506
MaxMainio <beidler.max@gmail.com>
2022-11-04 18:04:22 -0400

    test continuing

--- work/test/sources/test.js
+++ work/test/sources/test.js
@@ -1,1 +1,12 @@
-console.log("https://www.youtube.com/watch?v=NuAKnbIr6TE");
+window.onload = () => {
+    const transitionElement = document.querySelector(".fader-in");
+
+    const innerLinks = document.querySelectorAll('a:not([target="_blank"])');
+    console.log(innerLinks);
+
+    transitionElement.classList.remove("is-active");
+    
+    setTimeout(() => {
+        transitionElement.remove();
+    }, 500);
+}
