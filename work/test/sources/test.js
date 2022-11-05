b1f497fd0e3721a96426673d18c2061cfd02f5cf
MaxMainio <beidler.max@gmail.com>
2022-11-04 23:21:17 -0400

    Test pretty much finished contemplating full roll out...

--- work/test/sources/test.js
+++ work/test/sources/test.js
@@ -1,12 +1,31 @@
 window.onload = () => {
-    const transitionElement = document.querySelector(".fader-in");
-
+    const tulossaElement = document.querySelector(".fader-in");
     const innerLinks = document.querySelectorAll('a:not([target="_blank"])');
-    console.log(innerLinks);
 
-    transitionElement.classList.remove("is-active");
+    tulossaElement.classList.remove("tulossa");
     
     setTimeout(() => {
-        transitionElement.remove();
+        tulossaElement.remove();
     }, 500);
+
+    for (let i = 0; i < innerLinks.length; i++) {
+        const innerLink = innerLinks[i];
+
+        innerLink.addEventListener("click", e => {
+            e.preventDefault();
+
+            var target = e.target.href;
+            const menossaDiv = document.createElement("div");
+            menossaDiv.classList.add("fader-out");
+
+            document.body.append(menossaDiv);
+            setTimeout(() => {
+                menossaDiv.classList.add("menossa");
+            },1);
+            
+            setTimeout(() => {
+                window.location.href = target;
+            }, 499);
+        });
+    }
 }
