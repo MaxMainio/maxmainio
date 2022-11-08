57e23d12a0bb77755dc926ce4c894fad3e0870b8
MaxMainio <beidler.max@gmail.com>
2022-11-08 10:33:38 -0500

    small push

--- work/test/sources/testAddOnLoad.js
+++ work/test/sources/testAddOnLoad.js
@@ -1,15 +1,23 @@
 console.log("https://www.youtube.com/watch?v=NuAKnbIr6TE");
 
 window.onpageshow = () => {
+    // Entering page fade in
+    const tulossaDiv = document.createElement("div");
+    tulossaDiv.classList.add("fader-in", "tulossa");
+    document.body.append(tulossaDiv);
+
     const tulossaElement = document.querySelector(".fader-in");
+    setTimeout(() => {
+        tulossaElement.classList.remove("tulossa");
+
+        setTimeout(() => {
+            tulossaElement.remove();
+        }, 499);
+    }, 1);
+
     const innerLinks = document.querySelectorAll('a:not([target="_self"], [target="_blank"])');
 
-    tulossaElement.classList.remove("tulossa");
-    
-    setTimeout(() => {
-        tulossaElement.remove();
-    }, 500);
-
+    // Exiting page fade out
     for (let i = 0; i < innerLinks.length; i++) {
         const innerLink = innerLinks[i];
 
@@ -17,7 +25,6 @@ window.onpageshow = () => {
             e.preventDefault();
 
             var target = e.currentTarget.href;
-            console.log(target);
             const menossaDiv = document.createElement("div");
             menossaDiv.classList.add("fader-out");
 
