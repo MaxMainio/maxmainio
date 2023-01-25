6b9e0d7ed878a475a9b20c59c5ce0e4d6ed8c843
MaxMainio <beidler.max@gmail.com>
2023-01-25 11:56:35 -0500

    acetate vs2

---
+++ sources/fade-new.js
@@ -0,0 +1,65 @@
+console.log('https://www.youtube.com/watch?v=NuAKnbIr6TE');
+
+var vw = window.innerWidth
+const innerLinks = document.querySelectorAll('a:not([target="_self"], [target="_blank"])');
+const faderElement = document.querySelector('#fader');
+
+// TRIGGER FADER WHEN ENTERING AND EXITING EACH PAGE
+// if (vw > 800) {
+//     window.onpageshow = () => {
+//         faderElement.classList.remove('motion');
+
+//         setTimeout(() => {
+//             faderElement.remove();
+//         }, 500);
+
+//         innerLinks.forEach(item => {
+//             item.addEventListener('click', e => {
+//                 e.preventDefault();
+
+//                 var target = e.currentTarget.href;
+//                 const faderDiv = document.createElement('div');
+//                 faderDiv.classList.add('fader');
+
+//                 document.body.append(faderDiv);
+//                 setTimeout(() => {
+//                     faderDiv.classList.add('motion');
+//                 }, 1);
+
+//                 setTimeout(() => {
+//                     window.location.href = target;
+//                 }, 499);
+//             })
+//         })
+//     }
+// } else {
+//     faderElement.remove();
+// }
+
+var t = setInterval(fadeCheck,1);
+
+function fadeCheck() {
+    console.log('hello')
+    if (faderElement.classList.contains('motion')) {
+        faderElement.classList.remove('motion');
+
+        setTimeout(() => {
+            faderElement.remove();
+            clearInterval(t);
+        }, 500);
+    } else {
+        clearInterval(t);
+    }
+}
+
+
+
+
+
+
+
+
+
+addEventListener('click', e => {
+    clearInterval(t);
+})
