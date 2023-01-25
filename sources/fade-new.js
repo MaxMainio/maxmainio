57b477b972d653a7aae9def35c7cc1a80e8463cb
MaxMainio <beidler.max@gmail.com>
2023-01-25 12:10:20 -0500

    fade update test 2

--- sources/fade-new.js
+++ sources/fade-new.js
@@ -4,62 +4,63 @@ var vw = window.innerWidth
 const innerLinks = document.querySelectorAll('a:not([target="_self"], [target="_blank"])');
 const faderElement = document.querySelector('#fader');
 
-// TRIGGER FADER WHEN ENTERING AND EXITING EACH PAGE
-// if (vw > 800) {
-//     window.onpageshow = () => {
-//         faderElement.classList.remove('motion');
 
-//         setTimeout(() => {
-//             faderElement.remove();
-//         }, 500);
 
-//         innerLinks.forEach(item => {
-//             item.addEventListener('click', e => {
-//                 e.preventDefault();
 
-//                 var target = e.currentTarget.href;
-//                 const faderDiv = document.createElement('div');
-//                 faderDiv.classList.add('fader');
 
-//                 document.body.append(faderDiv);
-//                 setTimeout(() => {
-//                     faderDiv.classList.add('motion');
-//                 }, 1);
+innerLinks.forEach(item => {
+    item.addEventListener('click', e => {
+        e.preventDefault();
 
-//                 setTimeout(() => {
-//                     window.location.href = target;
-//                 }, 499);
-//             })
-//         })
-//     }
-// } else {
-//     faderElement.remove();
-// }
+        fadeOut(e);
+    })
+})
+
+
+
+
+
+
+
+
+
+
 
 var t = setInterval(fadeCheck,1);
 
 function fadeCheck() {
     console.log('hello')
     if (faderElement.classList.contains('motion')) {
-        faderElement.classList.remove('motion');
-
-        setTimeout(() => {
-            faderElement.remove();
-            clearInterval(t);
-        }, 500);
+        fadeIn();
     } else {
         clearInterval(t);
     }
+}
+
+function fadeIn() {
+    faderElement.classList.remove('motion');
+
+    setTimeout(() => {
+        faderElement.remove();
+        clearInterval(t);
+    }, 500);
 }
 
 
 
 
 
+function fadeOut(e){
+    var target = e.currentTarget.href;
+    const faderDiv = document.createElement('div');
+    faderDiv.classList.add('fader');
 
+    document.body.append(faderDiv);
+    setTimeout(() => {
+        faderDiv.classList.add('motion');
+    }, 1);
 
-
-
-addEventListener('click', e => {
-    clearInterval(t);
-})
+    setTimeout(() => {
+        window.location.href = target;
+    }, 499);
+}
