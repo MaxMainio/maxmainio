eefe5445333ab204d1964cfee2460cc46737bfbc
MaxMainio <beidler.max@gmail.com>
2023-01-30 16:55:59 -0500

    Digital overprint BIG update!

--- work/digital-overprint/sources/digital-overprint.js
+++ work/digital-overprint/sources/digital-overprint.js
@@ -1,3 +1,29 @@
 $( function() {
     $( ".draggable" ).draggable();
 } );
+
+
+
+
+
+
+
+
+
+const blends = [].slice.call(document.getElementsByName("blend-mode"));
+
+const docElements = document.querySelectorAll('.draggable');
+
+blends.forEach((radioBtn) => {
+    radioBtn.addEventListener('click', () => pickBlendMode(radioBtn, docElements));
+});
+
+const pickBlendMode = (blendMode, elem) => {
+    let chosenMode;
+    if (blendMode.checked) {
+        chosenMode = blendMode.value;
+        for (var i = 0, max = elem.length; i < max; i++) {
+            elem[i].style.setProperty('--blend-mode', chosenMode);
+        }
+    }
+};
