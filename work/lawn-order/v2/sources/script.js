589c58ccf67c7488efc1bc4f3cbbe66cfda2ac48
MaxMainio <beidler.max@gmail.com>
2023-04-24 20:02:59 -0400

    new lawn order 3 json

--- work/lawn-order/v2/sources/script.js
+++ work/lawn-order/v2/sources/script.js
@@ -11,7 +11,7 @@ const spaceWidth = document.getElementById('space').offsetWidth;
 
 
 /* JSON FETCH ----------------------------------------------------------------------------------------- */
-fetch('https://maxmain.io/work/lawn-order/v3/sources/data.json')
+fetch('https://maxmain.io/work/lawn-order/v2/sources/data.json')
     .then((response) => response.json())
     .then((json) => {
 
--- work/lawn-order/v3/sources/script.js
+++ work/lawn-order/v3/sources/script.js
@@ -3,3 +3,15 @@
 //     .then((response) => response.json())
 //     .then((json) => {
 // });
+
+window.addEventListener('click', event => {
+    window.open('../v2/index.html', '', 'menubar=no, status=no, height=200, width=400, scrollbars=no');
+});
+
+fetch('https://maxmain.io/work/lawn-order/v3/sources/data.json')
+    .then((response) => response.json())
+    .then((json) => {
+
+    highlightWords(json);
+    wrapRest();
+});
