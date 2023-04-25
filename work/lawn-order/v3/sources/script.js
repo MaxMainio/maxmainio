f697e993f7efb0d7ad190e834e3e2a6dccdf6a54
MaxMainio <beidler.max@gmail.com>
2023-04-25 10:07:05 -0400

    json updated yet again...

--- work/lawn-order/v3/sources/script.js
+++ work/lawn-order/v3/sources/script.js
@@ -1,16 +1,56 @@
-/* JSON FETCH ----------------------------------------------------------------------------------------- */
-// fetch('https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=your-api-key')
-//     .then((response) => response.json())
-//     .then((json) => {
+/* CONSTANTS ------------------------------------------------------------------------------------------ */
+const textField = document.querySelectorAll('p');
+const spaceWidth = document.getElementById('space').offsetWidth;
+
+
+
+
+
+
+
+
+
+/* POPUP WINDOW --------------------------------------------------------------------------------------- */
+// window.addEventListener('click', event => {
+//     window.open('../v2/index.html', '', 'menubar=no, status=no, height=200, width=400, scrollbars=no');
 // });
 
-window.addEventListener('click', event => {
-    window.open('../v2/index.html', '', 'menubar=no, status=no, height=200, width=400, scrollbars=no');
-});
 
+
+
+
+
+
+
+
+/* JSON FETCH ----------------------------------------------------------------------------------------- */
 fetch('https://maxmain.io/work/lawn-order/v3/sources/data.json')
     .then((response) => response.json())
     .then((json) => {
-        console.log(json);
-        console.log(json[0].plants[0]);
+        const plants = json[0];
+        const flowers = json[1];
+        const water = json[2];
+
+        highLightFlowers(flowers);
+        console.log(flowers);
 });
+
+
+
+
+
+
+
+
+
+function highLightFlowers(flowers){
+    for (let i = 0; i < flowers.length; i++) {
+        textField.forEach(element => {
+            // var replace = json[i].baseWord;
+            // const regex = new RegExp('(' + replace + ')', 'gi');
+            // element.innerHTML = element.innerHTML.replace(regex, '<span class="active col-' + getRandomInt(1,30) + '">$1</span>');
+
+            let replace = flowers[i];
+        });
+    };
+}
