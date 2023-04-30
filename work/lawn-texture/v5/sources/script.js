a6ea17c550a0351144364ab2852f15ac82e67c0c
MaxMainio <beidler.max@gmail.com>
2023-04-30 16:20:14 -0400

    highlights everything and wraps everything else

--- work/lawn-texture/v5/sources/script.js
+++ work/lawn-texture/v5/sources/script.js
@@ -51,14 +51,19 @@ fetch('https://maxmain.io/work/lawn-texture/v5/sources/data.json')
 
 
 
-
 function initialHighLight(lawn, water,flowers) {
-    // highlightWords(lawn);
-    // highlightWords(water);
+    highlightWords(lawn);
+    highlightWords(water);
     highlightGlyphs(flowers);
 
-    // wrapWrest();
+    wrapRest();
 };
+
+
+
+
+
+
 
 
 
@@ -73,19 +78,84 @@ function highlightWords(currentPass) {
             const regex = new RegExp('((?<!<[^>]+>)' + replace + '(?![^<]*>))', 'gi');
             let colorNum = getRandomInt(0, (colors.length) - 1);
 
-            element.innerHTML = element.innerHTML.replace(regex, '<span class="lawn" style="background-color: ' + colors[colorNum] + ';" data-color="' + colorNum + '">$1</span>');
+            element.innerHTML = element.innerHTML.replace(regex, '<span class="' + subject + '" style="background-color: ' + colors[colorNum] + ';" data-color="' + colorNum + '">$1</span>');
         })
     };
 };
+
+
+
+
+
+
+
+
 
 function highlightGlyphs(currentPass) {
     const subject = currentPass.key;
     const search = currentPass.words;
     const types = currentPass.types;
+    const typeKeys = Object.keys(types);
+
+
+
+    const clumps = defineClumps(search);
+
+    for (let i = 0; i < clumps.length; i++) {
+        splitClumps(clumps[i], types, typeKeys);
+    }
+
+
+    clumps.forEach(element => element.removeAttribute('class'));
 };
 
 
 
+function defineClumps(search) {
+    for (let i = 0; i < search.length; i++) {
+        textField.forEach(element => {
+            let replace = search[i];
+            const regex = new RegExp('((?<!<[^>]+>)' + replace + '(?![^<]*>))', 'gi');
+
+            element.innerHTML = element.innerHTML.replace(regex, '<span class="clump">$1</span>');
+        })
+    };
+    
+    let clumps = document.querySelectorAll('.clump');
+    return (clumps);
+};
+
+
+
+function splitClumps(clump, types, typeKeys) {
+    let replace = clump.innerHTML;
+    const regex = new RegExp('([' + replace + '])', 'gi');
+    let split = clump.innerHTML.match(regex);
+
+    let chosenType = typeKeys[getRandomInt(0, (typeKeys.length - 1))];
+    let colors = types[chosenType];
+
+    for (let i = 0; i < split.length; i++) {
+        split[i] = '<span class="flower" style="background-color: ' + colors[getRandomInt(0, (colors.length) - 1)] + ';" data-color="' + chosenType + '">' + split[i] + '</span>';
+    };
+
+    let newHTML = split.join('');
+    clump.innerHTML = newHTML;
+};
+
+
+
+
+
+
+
+
+
+function wrapRest() {
+    textField.forEach(element => {
+        element.innerHTML = element.innerHTML.replace(/((?<!<[^>]+>)\b\w+\b(?![^<]*>))/gi, '<span>$1</span>');
+    });
+};
 
 
 
