94fb0227a4d625d15d1305ba73c9892a1508f4d1
Max Mainio Beidler <beidler.max@gmail.com>
2023-06-18 19:35:27 -0700

    it's better now. Constant change in background and interactive swap now live

--- work/seam-carving/sources/seam-carving.js
+++ work/seam-carving/sources/seam-carving.js
@@ -1,5 +1,6 @@
-const contents = document.getElementsByClassName('content-container');
+const contents = [].slice.call(document.getElementsByClassName('content-container'));
 const contentObjs = [];
+
 const altTexts = {
     1: "the transformation the city’s greenery—parks, forests, and other natural areas—seemingly dissolves away as if subtly edged out of existence. This visual shift represents a real-world human mindset: the prioritization of human living space over natural landscapes. When the image expands, revealing newfound space, the instinct isn't to restore nature. Instead, it's seen as room for more urban development, reinforcing the narrative of human dominance and expansion over nature.",
     10: "this change, the city's urban infrastructure—buildings, streets, and other man-made structures—seemingly vanish as if subtly pushed out of existence. This visual shift represents a counter-narrative to the real-world human mindset: the prioritization of nature over human living space. When the image expands, revealing newfound space, the instinct isn't to fill it with more urban development. Instead, it's seen as an opportunity for nature to reclaim and flourish within the newly opened space, challenging the typical narrative of human dominance and expansion over nature."
@@ -29,7 +30,10 @@ window.onload = (event) => {
 
 var intervalId = window.setInterval(function(){
 	counter ++
-    updateImgs(counter);
+
+    for (let i = 0; i < contents.length; i++) {
+        updateCollectiveImages(counter);
+    };
 }, 1500);
 
 
@@ -46,8 +50,8 @@ function prepObjects() {
         let obj = {
             location: current.id,
             source: 'assets/maps/' + current.id + '/' + current.id + '-',
-            alt: 'Starting with a satellite view of ' + current.id + ', unaltered in the first frame, the image then undergoes a striking transformation, compressing horizontally, and with ',
-            priority: '1',
+            alt: 'Animated triplet of images, starting with a satellite view of ' + current.id + ', unaltered in the first frame, the image then undergoes a striking transformation, compressing horizontally, and with ',
+            priority: '1'
         };
     
         obj.alt = obj.alt + altTexts[obj.priority];
@@ -61,7 +65,7 @@ function prepObjects() {
 
 
 
-function updateImgs(counter) {
+function updateCollectiveImages(counter) {
     for (let i = 0; i < contents.length; i++) {
         let current = contents[i].children[0].children[0];
         let individualCounter = counter - i;
@@ -80,12 +84,31 @@ function updateImgs(counter) {
 
 
 
-function changePriority(element, value) {
-    let index = element.dataset.index;
+function changePriority(current, value) {
+    let index = current.dataset.index;
 
     if (value === 'nature') {
         contentObjs[index].priority = 10;
     } else {
         contentObjs[index].priority = 1;
     };
-}
+
+    updateSingleImage(current, contentObjs[index], counter);
+};
+
+
+
+function updateSingleImage(current, info, counter) {
+    let individualCounter = counter - current.dataset.index;
+
+    if (individualCounter % 4 === 0) {
+        var frame = 0;
+    } else if (individualCounter % 4 === 2) {
+        var frame = (individualCounter % 4) * info.priority;
+    } else {
+        var frame = info.priority;
+    };
+
+    current.src = info.source + frame + '.jpg';
+    current.alt = 'Animated triplet of images, starting with a satellite view of ' + info.location + ', unaltered in the first frame, the image then undergoes a striking transformation, compressing horizontally, and with ' + altTexts[info.priority];
+};
