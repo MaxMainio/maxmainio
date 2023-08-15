e91d1e3ae62f3c9cb86fc98f5477ded5d4b5faaf
Max Mainio Beidler <beidler.max@gmail.com>
2023-08-15 13:40:54 -0700

    interview fix

--- work/ed-brown-interview/sources/interview.js
+++ work/ed-brown-interview/sources/interview.js
@@ -1,24 +1,22 @@
 const images = document.querySelectorAll('.work-img');
 
-console.log(images);
-
 images.forEach(function (item){
     item.addEventListener("mousedown", (event) => {
         let current = event.target;
         let container = event.target.parentNode.parentNode;
 
-        current.setAttribute('id','focused');
-        container.setAttribute('id', 'focusedcontainer');
+        current.classList.add('focused');
+        container.classList.add('focusedcontainer');
 
         document.body.style.cursor = 'zoom-in';
 
 
 
         document.addEventListener("mouseup", (event) => {
-            current.removeAttribute('id', 'focused');
-            container.removeAttribute('id', 'focusedcontainer');
+            current.classList.remove('focused');
+            container.classList.remove('focusedcontainer');
 
-            document.body.style.cursor = 'auto';
+            document.body.removeAttribute('style');
         });
     });
 });
