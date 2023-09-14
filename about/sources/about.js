fce16e735974065cb0caa0fa293ae23c1a71026c
Max Mainio Beidler <beidler.max@gmail.com>
2023-09-15 00:05:08 +0300

    little house keeping

--- about/sources/about.js
+++ about/sources/about.js
@@ -1,15 +1,58 @@
 // EXPERIENCE TOGGLES
-var allXpBtns = document.getElementsByClassName('xp-btn');
+const allXpBtns = document.getElementsByClassName('xp-btn');
 
 for (var i = allXpBtns.length-1; i >=0  ; i--) {
 	allXpBtns[i].addEventListener('click', function() {
-		var modal = this.id + '_modal'
-		document.getElementById(modal).classList.toggle('hide')
+		var modal = this.id + '_modal';
+		var container = this.parentNode.previousElementSibling;
+		var blocks = container.querySelectorAll('.modal');
 
+		blocks.forEach(element => {
+			element.classList.toggle('hide');
+		});
+		
 		if (this.innerHTML === 'More...') {
 			this.innerHTML = 'Less...'
 		} else {
 			this.innerHTML = 'More...'
-		}
-	})
-}
+		};
+	});
+};
+
+
+
+
+
+
+
+
+// FOOTER FIX
+const footer = document.getElementById('footer-nav').parentNode.parentNode;
+const xpSection = document.querySelector('.xp-section');
+
+window.addEventListener('load', setFooter());
+window.addEventListener('resize', event => {
+	setFooter();
+});
+
+
+
+function setFooter(){
+	let vw = checkVW();
+	if(vw >= 1000){
+		let fix = getHight(footer);
+		xpSection.style.minHeight = 'calc(100vh - 3rem - ' + fix + 'px)';
+	} else {
+		xpSection.removeAttribute('style')
+	};
+};
+
+function checkVW(){
+	let vw = window.innerWidth;
+	return(vw);
+};
+
+function getHight(footer){
+	let height = footer.offsetHeight;
+	return(height);
+};
