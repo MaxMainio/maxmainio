d3f6d5dc13778612edab2b7bbec08e04826f6eaf
Max Mainio Beidler <beidler.max@gmail.com>
2023-10-17 09:42:02 -0700

    started prepping js

--- work/crosswalk-poetics/sources/crosswalk-poetics.js
+++ work/crosswalk-poetics/sources/crosswalk-poetics.js
@@ -1,2 +1,7 @@
 // GLOBAL VARIABLES & CONSTANTS ====================================================================================================
 // Values & measurements    --------------------------------------------------------------------------------------------------------
+const horizontalIMGs = 10;
+const verticalIMGs = 17;
+const squareIMGs = 25;
+
+const path = 'assets/no-index/poem-imagery/'
